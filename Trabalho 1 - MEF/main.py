6c18e6d1617f4750d574236f0a2fdc1e21474629
Enzo Campa <enzocampa44@gmail.com>
2022-08-10 21:04:26 -0300

    Atualização do código
    Mudança nas funções utilizadas no código

--- Trabalho 1 - MEF/main.py
+++ Trabalho 1 - MEF/main.py
@@ -1,18 +1,12 @@
-#Parâmetros
-
-alphabet = ['a', 'b', 'c']
-
-txt = open("test03.txt", 'r')
-
-#funções de estado
+#---Funções de estado---
 
 def State1(txt, index):
   if(len(txt) <= index):
     return printResult(txt, True)
-  if(txt[index] == 'b' or txt[index] == 'c'):
+  if(txt[index] == 'b'):
     return State1(txt, index+1)
   if(txt[index] == 'a'):
-    return State2(txt, index+1)
+    return State2(txt, index+1)  
   return State4(txt)
   
 def State2(txt, index):
@@ -20,8 +14,7 @@ def State2(txt, index):
     return State4(txt)
   if(txt[index] == 'b'):
     return State3(txt, index+1)
-  return State4(txt)
-    
+  return State4(txt)  
 
 def State3(txt, index):
   if(len(txt) <= index):
@@ -33,20 +26,30 @@ def State3(txt, index):
 def State4(txt):
   return printResult(txt, False)
 
-#funções Utilitárias 
+#---funções Utilitárias---
 
 def printResult(txt, val):
   if(val):
-    return print(txt,": Pertence", sep="")
-  return print(txt,": Não pertence", sep="")
+    return print(txt,": Pertence.", sep="")
+  return print(txt,": Não pertence.", sep="")
 
-#body
-  
-lineCount = int(txt.readline())
+def fileReader(text):
+  lineCount = int(text.readline())
 
-for line in range(lineCount):
-  text = txt.readline()
-  text2 = text.rstrip('\n')
-  State1(text2, 0)
-  
-txt.close()
+  for x in range(lineCount):
+    textLine = text.readline()
+    textNoBreak = textLine.rstrip('\n')
+    State1(textNoBreak, 0)
+  text.close()
+
+#---Parâmetros---
+
+txt1 = open("test01.txt", 'r')
+txt2 = open("test02.txt", 'r')
+txt3 = open("test03.txt", 'r')
+
+#---Chamada das funções---
+
+fileReader(txt1)
+fileReader(txt2)
+fileReader(txt3)
